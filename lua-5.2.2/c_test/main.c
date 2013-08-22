3b0abbbdebaf87504d1c92cfbd9bcebb8b96251d
Arijit Chattopahdyay <arijitnit06@gmail.com>
2013-08-22 02:32:05 -0400

    Adding lua test files.

---
+++ lua-5.2.2/c_test/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+int main() {
+
+	lua_State *l = luaL_newstate();
+
+	return 0;
+}
